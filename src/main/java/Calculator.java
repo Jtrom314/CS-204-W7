83f379129b1f01f606a00d2b1bb068f2f8bf2ffc
Jtrom314 <97546727+Jtrom314@users.noreply.github.com>
2025-01-03 09:23:28 -0700

    moved file

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -20,9 +20,6 @@ class Calculator {
     }
 
     int divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
         return a / b;
     }
 
